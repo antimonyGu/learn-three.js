7306907c65307ed84bc23ce6caee93d5fea24668
guzhizhou <guzhizhou28@gmail.com>
2020-03-21 11:37:52 +0800

    use npm to run webpack to build the page

---
+++ 02-use-npm/webpack.config.js
@@ -0,0 +1,14 @@
+'use strict'
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './src/index.html'})
+    ],
+    mode: 'production'
+}
