355bafec9b7a5e62b6e2c6d0d5e3044a0e9bd4df
guzhizhou <guzhizhou28@gmail.com>
2020-03-25 22:59:04 +0800

    finish the drinking bird exercise

--- 06/src/index.js
+++ 06/src/index.js
@@ -1,3 +1,4 @@
 // import creation from './polygon-location.js'
 // import radius from './polygon-radius.js'
-import stairway from './build-a-stairway.js'
+// import stairway from './build-a-stairway.js'
+import drinkingBird from './the-drinking-bird.js'
