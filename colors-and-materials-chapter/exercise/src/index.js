9643903f8e70d3968f05d7d046ae9313b1515501
guzhizhou <guzhizhou28@gmail.com>
2020-04-01 22:34:39 +0800

    change the glassMaterial into transparent

--- colors-and-materials-chapter/exercise/src/index.js
+++ colors-and-materials-chapter/exercise/src/index.js
@@ -1,2 +1,3 @@
 // import vertex from './vertex-attributes.js';
-import diffuseMaterial from './diffuse-material.js'
+// import diffuseMaterial from './diffuse-material.js';
+import drinkingBirdWithWater from './drinking-bird-with-water.js';
