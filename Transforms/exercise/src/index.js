c0e2b0168ef5ea1d79a5b6dbbdcfe62a69f96ccf
guzhizhou <guzhizhou28@gmail.com>
2020-04-05 23:36:30 +0800

    can use dashboard to control the ratation and translation of robot's hand

--- Transforms/exercise/src/index.js
+++ Transforms/exercise/src/index.js
@@ -1,4 +1,5 @@
 // import rotateBlock from './rotate-block';
 // import buildASnowman from './build-a-snowman';
 // import fixWrongOrder from './fix-wrong-order';
-import extendedRobortArm from './extended-robort-arm.js';
+// import extendedRobortArm from './extended-robort-arm.js';
+import robotHand from './robot-hand.js';
