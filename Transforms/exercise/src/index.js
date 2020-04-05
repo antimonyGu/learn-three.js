eef5c0978d2154e9138064ab5b53c649280e9755
guzhizhou <guzhizhou28@gmail.com>
2020-04-05 21:03:32 +0800

    extend the robort arm and learn how to use dat.gui

--- Transforms/exercise/src/index.js
+++ Transforms/exercise/src/index.js
@@ -1,3 +1,4 @@
 // import rotateBlock from './rotate-block';
 // import buildASnowman from './build-a-snowman';
-import fixWrongOrder from './fix-wrong-order';
+// import fixWrongOrder from './fix-wrong-order';
+import extendedRobortArm from './extended-robort-arm.js';
