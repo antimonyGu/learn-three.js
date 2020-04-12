2a7a8b19aa78e3b5669a09431e740a05b5ffab01
guzhizhou <guzhizhou28@gmail.com>
2020-04-12 23:00:48 +0800

    finish drinking bird's final version

--- Matrices/exercise/src/index.js
+++ Matrices/exercise/src/index.js
@@ -1,4 +1,5 @@
 // import makeAOrnament from './make-a-ornament';
 // import cylinderPositioning from './cylinder-positioning';
 // import capsule from './capsule';
-import helices from './helices';
+// import helices from './helices';
+import drinkingBird from './revisiting-the-drinking-bird'
