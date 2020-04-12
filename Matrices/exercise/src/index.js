979daf6ce367116868b876be222c1d5b7390e733
guzhizhou <guzhizhou28@gmail.com>
2020-04-12 10:34:54 +0800

    finish capsule exercise

--- Matrices/exercise/src/index.js
+++ Matrices/exercise/src/index.js
@@ -1,2 +1,3 @@
 // import makeAOrnament from './make-a-ornament';
-import cylinderPositioning from './cylinder-positioning';
+// import cylinderPositioning from './cylinder-positioning';
+import capsule from './capsule';
